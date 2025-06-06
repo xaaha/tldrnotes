a3536a034db5916290ace2639e71f6962a728587
xaaha <pratik.philos@yahoo.com>
2025-06-06 17:04:59 -0400

    remove console statements

--- src/scripts/nav.ts
+++ src/scripts/nav.ts
@@ -1,9 +1,6 @@
 const navLinks = document.querySelectorAll("[data-navLink]");
-console.log("📮", navLinks);
 for (const link of navLinks) {
   if (link.getAttribute("href") === window.location.pathname) {
     link.setAttribute("aria-current", "page");
   }
 }
-
-console.log("📮", navLinks);
