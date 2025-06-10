79c2f14a141758def7560c3f617ce3ebf3598f95
xaaha <pratik.philos@yahoo.com>
2025-06-10 17:40:03 -0400

    dynamic render blog posts

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,6 +1,13 @@
 declare var myString: string;
 declare function myFunction(): boolean;
 
-// interface Window {
-//   myFunction(): boolean;
-// }
+// blog's
+interface Frontmatter {
+  title: string;
+  date: string;
+  author?: string;
+  image?: { src: string; alt: string };
+  description?: string;
+  draft?: boolean;
+  category?: string;
+}
