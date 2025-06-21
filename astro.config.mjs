e5b0613760970d1f6a17e38d66da9dd3b785d03d
Pratik Thapa <pratik.philos@yahoo.com>
2025-06-21 13:02:10 -0400

    use tokyo night and not pure black for background

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,7 +9,7 @@ export default defineConfig({
   markdown: {
     remarkPlugins: [remarkCallout],
     shikiConfig: {
-      theme: "one-dark-pro",
+      theme: "tokyo-night",
       // wrap: true,
     },
   },
