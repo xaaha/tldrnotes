ca4c2a1c299e8621cc07c9410b8e07da178d3475
xaaha <pratik.philos@yahoo.com>
2025-06-28 19:08:56 -0400

    style fixes for load more button

--- src/components/PostList.tsx
+++ src/components/PostList.tsx
@@ -38,21 +38,17 @@ const PostList: FC<PostListProps> = ({ posts }) => {
             className="load-more-button"
             type="button"
           >
-            <span>Load More</span>
+            <span>Load More Notes</span>
             <svg
+              width="20"
+              height="20"
+              viewBox="0 0 20 20"
               xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              strokeWidth="2"
-              strokeLinecap="round"
-              strokeLinejoin="round"
             >
-              <title>Load More</title>
-              <path d="M12 5v14m-7-7h14" />
-              <path d="m19 12-7 7-7-7" />
+              <path
+                fill="currentColor"
+                d="M10.103 12.778L16.81 6.08a.69.69 0 0 1 .99.012a.726.726 0 0 1-.012 1.012l-7.203 7.193a.69.69 0 0 1-.985-.006L2.205 6.72a.727.727 0 0 1 0-1.01a.69.69 0 0 1 .99 0z"
+              />
             </svg>
           </button>
         </div>
