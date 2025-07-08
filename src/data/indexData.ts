e6f89c3f4cfc14a0c3cb1cfec46f01fe58da8391
Pratik Thapa <pratik.philos@yahoo.com>
2025-07-08 16:15:51 -0400

    start addign the title description and stuff

---
+++ src/data/indexData.ts
@@ -0,0 +1,10 @@
+const hero = {
+  subtitle: "To stay sane, I work on side projects that add value to my life.",
+  aside: "And I use Neovim, by the way.",
+};
+
+const projectInfo = {
+  title: "Hulak",
+  description:
+    "I needed a lightweight, dependency-free API client for my terminal. So, I built Hulak - a CLI tool written in Go that provides a fast and minimal API client that can be defined in yaml file.",
+};
